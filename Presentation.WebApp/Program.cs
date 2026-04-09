01f04d58a61e6d6632e4dce953e86aa8984b478f
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-04-09 12:06:25 +0200

    Refactored folder structure and added layout file

--- Presentation.WebApp/Program.cs
+++ Presentation.WebApp/Program.cs
@@ -3,7 +3,6 @@ using Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
@@ -11,14 +10,7 @@ builder.Services.AddApplication(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+app.UseHsts();
 app.UseHttpsRedirection();
 app.UseRouting();
 
