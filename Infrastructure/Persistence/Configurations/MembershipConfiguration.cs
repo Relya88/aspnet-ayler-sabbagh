1aa4e763d660883e7bba35d57703f970cbcda651
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-04-13 12:14:02 +0200

    Configured MembershipEntity and verified database setup (removed NotImplementedException)

--- Infrastructure/Persistence/Configurations/MembershipConfiguration.cs
+++ Infrastructure/Persistence/Configurations/MembershipConfiguration.cs
@@ -6,8 +6,20 @@ namespace Infrastructure.Persistence.Configurations;
 
 internal class MembershipConfiguration : IEntityTypeConfiguration<MembershipEntity>
 {
+    //fick ändra metoden helt från throw new NotImplementedException då jag fick mycket error.
+    //tog hjälp av chatgpt
     public void Configure(EntityTypeBuilder<MembershipEntity> builder)
     {
-        throw new NotImplementedException();
+        builder.HasKey(x => x.Id); // satte primary key
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Price)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(500);
     }
 }
--- Presentation.WebApp/Controllers/MembershipsController.cs
+++ Presentation.WebApp/Controllers/MembershipsController.cs
@@ -1,6 +1,24 @@
-﻿namespace Presentation.WebApp.Controllers
+﻿using Application.Memberships;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Models;
+
+namespace Presentation.WebApp.Controllers;
+
+// hanterar medlemssidor
+public class MembershipsController(IMembershipService service) : Controller
 {
-    public class MembershipsController
+    private readonly IMembershipService _service = service;
+
+    //ska visa alla medlmeskap
+    public async Task<IActionResult> Index()
     {
+        var memberships = await _service.GetAllAsync();
+
+        var viewModel = new MembershipViewModel
+        {
+            Memberships = memberships
+        };
+
+        return View(viewModel);
     }
 }
