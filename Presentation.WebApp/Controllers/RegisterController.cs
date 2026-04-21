3354b73fd5b65063afb3baa8b4cb6a3ab8bef9b6
Ayler <ayler.sabbagh@gmail.com>
2026-04-21 15:51:03 +0200

    Added user registration with Identity (RegisterViewModel, controller, view)

--- Presentation.WebApp/Controllers/RegisterController.cs
+++ Presentation.WebApp/Controllers/RegisterController.cs
@@ -1,13 +1,46 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Models;
 
 namespace Presentation.WebApp.Controllers;
 
-// hanterar registrering
-public class RegisterController : Controller
+// hanterar registreringen av användare via identiity
+public class RegisterController(UserManager<IdentityUser> userManager) : Controller
 {
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+
     // visar registreringssidan
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
+    }
+
+    // tar emot formulärdata och skapar användare
+    [HttpPost]
+    public async Task<IActionResult> Index(RegisterViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var user = new IdentityUser
+        {
+            UserName = model.Email,
+            Email = model.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Index", "Login");
+        }
+
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        return View(model);
     }
 }
---
+++ Presentation.WebApp/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.WebApp.Models;
+
+// används för att ta emot data från registreringsformuläret
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [Compare(nameof(Password))]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = null!;
+}
