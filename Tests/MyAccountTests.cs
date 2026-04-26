bf547dd4a3f7d726ac2516a6d55075599a737abe
Ayler <ayler.sabbagh@gmail.com>
2026-04-26 17:26:01 +0200

    update MyAccountTests to use ApplicationUser and validate profile fields

--- Tests/MyAccountTests.cs
+++ Tests/MyAccountTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Infrastructure.Persistence.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -13,24 +14,25 @@ public class MyAccountTests
     public async Task Index_Post_ValidModel_UpdatesUser()
     {
         // Arrange
-        var user = new IdentityUser
+        var user = new ApplicationUser
         {
             Id = "1",
             Email = "old@mail.com",
-            UserName = "old@mail.com"
+            UserName = "old@mail.com",
+            FirstName = "Old",
+            LastName = "Name"
         };
 
         var userManagerMock = MockUserManager(user);
 
-        var signInManagerMock = new Mock<SignInManager<IdentityUser>>(
-        userManagerMock.Object,
-        Mock.Of<Microsoft.AspNetCore.Http.IHttpContextAccessor>(),
-        Mock.Of<IUserClaimsPrincipalFactory<IdentityUser>>(),
-        null, null, null, null);
+        var signInManagerMock = new Mock<SignInManager<ApplicationUser>>(
+            userManagerMock.Object,
+            Mock.Of<IHttpContextAccessor>(),
+            Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(),
+            null, null, null, null);
 
         var controller = new MyAccountController(userManagerMock.Object, signInManagerMock.Object);
 
-        // fake logged-in user
         var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
             new Claim(ClaimTypes.NameIdentifier, "1")
@@ -44,6 +46,8 @@ public class MyAccountTests
         var model = new MyAccountViewModel
         {
             Id = "1",
+            FirstName = "New",
+            LastName = "Person",
             Email = "new@mail.com",
             PhoneNumber = "123456"
         };
@@ -53,18 +57,23 @@ public class MyAccountTests
 
         // Assert
         Assert.Equal("new@mail.com", user.Email);
+        Assert.Equal("new@mail.com", user.UserName);
+        Assert.Equal("123456", user.PhoneNumber);
+        Assert.Equal("New", user.FirstName);
+        Assert.Equal("Person", user.LastName);
     }
 
-    private Mock<UserManager<IdentityUser>> MockUserManager(IdentityUser user)
+    private Mock<UserManager<ApplicationUser>> MockUserManager(ApplicationUser user)
     {
-        var store = new Mock<IUserStore<IdentityUser>>();
-        var mgr = new Mock<UserManager<IdentityUser>>(
+        var store = new Mock<IUserStore<ApplicationUser>>();
+
+        var mgr = new Mock<UserManager<ApplicationUser>>(
             store.Object, null, null, null, null, null, null, null, null);
 
         mgr.Setup(x => x.GetUserAsync(It.IsAny<ClaimsPrincipal>()))
             .ReturnsAsync(user);
 
-        mgr.Setup(x => x.UpdateAsync(It.IsAny<IdentityUser>()))
+        mgr.Setup(x => x.UpdateAsync(It.IsAny<ApplicationUser>()))
             .ReturnsAsync(IdentityResult.Success);
 
         return mgr;
